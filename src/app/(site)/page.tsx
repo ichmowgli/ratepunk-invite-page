233d86bd5dbb4d8d69eb0abe6e74caf21b207c89
Mariia Kucheieva <mary.kucheeva@gmail.com>
2023-06-11 02:21:50 +0300

    adaptive: Steps + minor changes

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,15 +1,40 @@
 import ReferalCard from "@/components/main_content/ReferalCard";
-import Step from "@/components/main_content/Step";
+import Step, { type IStep } from "@/components/main_content/Step";
+
+const steps: IStep[] = [
+  {
+    label: "Invite friends",
+    description: "Refer friends with your unique referral link",
+    image: "/images/invite.svg",
+  },
+  {
+    label: "Collect coins",
+    description:
+      "Get 1 coin for each friend that installs our extension using your referral link.",
+    image: "/images/collect-coins.svg",
+  },
+  {
+    label: "Get voucher",
+    description:
+      "Redeem for a $20 hotel booking voucher once you collect 20 coins.",
+    image: "/images/voucher.svg",
+  },
+];
 
 export default function Home() {
   return (
-    <div className="bg-[#F8CC5D] ">
-      <div className="mx-auto flex max-w-[1120px] flex-col items-center px-4 py-8 md:py-16 xl:flex-row xl:justify-between xl:gap-x-32">
+    <div className="bg-[#F8CC5D]">
+      <div className="mx-auto flex max-w-[1120px] flex-col items-center gap-y-12 px-4 py-8 md:px-0 md:py-16 xl:flex-row xl:justify-between xl:gap-x-32">
         <ReferalCard />
-        <div className="grid gap-y-12 pt-12">
-          <Step idx={1} />
-          <Step idx={2} />
-          <Step idx={3} />
+        <div className="grid gap-y-12">
+          {steps.map((step, index) => (
+            <Step
+              key={index}
+              index={index + 1}
+              {...step}
+              imagePosition={index % 2 ? "right" : "left"}
+            />
+          ))}
         </div>
       </div>
     </div>
--- src/components/main_content/Step.tsx
+++ src/components/main_content/Step.tsx
@@ -1,50 +1,36 @@
 import Image from "next/image";
 
-type Step = {
+export type IStep = {
   label: string;
   description: string;
   image: string;
 };
 
-const steps: Step[] = [
-  {
-    label: "Invite friends",
-    description: "Refer friends with your unique referral link",
-    image: "/images/invite.svg",
-  },
-  {
-    label: "Collect coins",
-    description:
-      "Get 1 coin for each friend that installs our extension using your referral link.",
-    image: "/images/collect-coins.svg",
-  },
-  {
-    label: "Get voucher",
-    description:
-      "Redeem for a $20 hotel booking voucher once you collect 20 coins.",
-    image: "/images/voucher.svg",
-  },
-];
-
-interface StepProps {
-  idx: number;
-}
-
-const Step: React.FC<StepProps> = ({ idx }) => {
-  const step = steps[idx - 1];
+const Step = (
+  step: IStep & { index: number; imagePosition: "left" | "right" }
+) => {
+  const rowDirectionClass =
+    step.imagePosition === "left" ? "" : "xl:flex-row-reverse";
   return (
-    <div className="flex flex-col items-center gap-8 text-[#1F343E] sm:flex-row">
+    <div
+      className={
+        "flex flex-col items-center gap-8 text-[#1F343E] sm:mx-auto sm:flex-row sm:gap-12  " +
+        rowDirectionClass
+      }
+    >
       <Image
         src={step.image}
-        alt="email"
+        alt={step.label}
         className="block cursor-pointer"
         width="144"
         height="144"
       />
-      <div className="flex flex-col gap-y-4 text-center sm:text-left">
-        <h4 className="font-bold uppercase text-[#4EB3E3] ">Step {idx}</h4>
+      <div className="flex flex-col  gap-y-4 text-center sm:max-w-[256px] sm:text-left">
+        <h4 className="font-bold uppercase text-[#4EB3E3] md:text-xl">
+          Step {step.index}
+        </h4>
         <h3 className="font-caveatBrush text-[32px] uppercase">{step.label}</h3>
-        <p>{step.description}</p>
+        <p className="md:text-xl">{step.description}</p>
       </div>
     </div>
   );
